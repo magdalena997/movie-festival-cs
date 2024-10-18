05ddf9743ca9dac21d7da020b4a79d33c13313b0
Magdalena Stankovic <magdalena.stankovic@traceone.com>
2024-10-18 09:29:35 +0200

    Add Movie Class

---
+++ MovieFestival/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieFestival
+{
+	public class Movie
+	{
+		public string Title { get; set; }
+		public string Genre { get; set; }
+		public int Length { get; set; }
+
+		public Movie(string title, string genre, int length)
+		{
+			Title = title;
+			Genre = genre;
+			Length = length;
+		}
+
+		public string GetData()
+		{
+			string acronym = !string.IsNullOrEmpty(Genre) ? $"{Genre[0]}{Genre[^1]}" : "";
+
+			return string.Format("Title: {0}, Length: {1}, Genre {2}", Title, Length, acronym);
+		}
+	}
+}
