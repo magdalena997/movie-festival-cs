e1a92a1dbaecf9cec8ee93574627065b10009682
Magdalena Stankovic <magdalena.stankovic@traceone.com>
2024-10-18 09:12:04 +0200

    Add Genre class

---
+++ MovieFestival/Genre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieFestival
+{
+	public class Genre
+	{
+		public string Name { get; set; }
+
+		public Genre(string name){
+			Name = name;
+		}
+
+		public string GetData(){
+			return !string.IsNullOrEmpty(Name) ? $"{Name[0]}{Name[^1]}" : "";
+		}
+
+	}
+}
